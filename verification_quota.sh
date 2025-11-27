50385df9f89a9c04ef6835682821e0e89799b96a
Hanitriniala7 <andriamahefahanitriniala@gmail.com>
2025-11-27 23:53:55 +0300

    Update verification_quota.sh

--- verification_quota.sh
+++ verification_quota.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-#verification /home
+#verification /home si soft dépassé
 
 verification_home() #partition /home
 {
  nombre_de_depassement=$(repquota /home | grep "+" | wc -l) #soft dépassé
 
+ if [ "$nombre_de_depassement" -eq 0 ] ; then
+      exit
+ fi     
+
   for i in $(repquota /home | grep "+") ; do
         
-         read user used soft hard grace
+         read user used soft hard grace 
          echo "Vous avez depassez la limite imposée ,vous occupé l'espace de tolérance de $soft vous avez $grace pour libérer cette espace " | mail -s "Alerte quota" $user
 
 }
 
 
-#verification data
+#verification /data si soft dépassé
 
 verification_data() #partition /data
 {
 
    nombre_de_depassement=$(repquota /data | grep "+" | wc -l) #soft dépassé
+   
+     if [ "$nombre_de_depassement" -eq 0 ] ; then
+            exit
+      fi     
    
    for i in $(repquota /data | grep "+") ; do #pour les users
    
@@ -27,8 +35,12 @@ verification_data() #partition /data
           echo "Vous avez depassez la limite imposée ,vous occupé l'espace de tolérance de $soft vous avez $grace pour libérer cette espace " | mail -s "Alerte quota" $user
    
           
-   nombre_de_depassement=$(repquota /data | grep "+" | wc -l) #soft dépassé
-   
+   nombre_de_depassement=$(repquota -g /data | grep "+" | wc -l) #soft dépassé
+
+      if [ "$nombre_de_depassement" -eq 0 ] ; then
+            exit
+      fi     
+      
    for i in $(repquota -g /data | grep "+") ; do #pour les groupes
    
           read user used soft hard grace
@@ -39,5 +51,6 @@ verification_data() #partition /data
 verification_data
 verification_home
 
-
+#Ce script est lancé 1 fois par semaine pour voir le si l'utilisateur/groupe ne dépassent pas le quota qui leur est attribué
+#si soft atteint envoie de mail à l'utilisateur 
 
