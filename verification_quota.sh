a9f817862182a1db8492c8761b4b9ea1df1dc6be
Hanitriniala7 <andriamahefahanitriniala@gmail.com>
2025-11-28 23:26:15 +0300

    Update verification_quota.sh

--- verification_quota.sh
+++ verification_quota.sh
@@ -1,56 +1,98 @@
 #!/bin/bash
 
-#verification /home si soft dépassé
+#verification /home
 
 verification_home() #partition /home
 {
- nombre_de_depassement=$(repquota /home | grep "+" | wc -l) #soft dépassé
 
- if [ "$nombre_de_depassement" -eq 0 ] ; then
-      exit
- fi     
-
-  for i in $(repquota /home | grep "+") ; do
-        
-         read user used soft hard grace 
-         echo "Vous avez depassez la limite imposée ,vous occupé l'espace de tolérance de $soft vous avez $grace pour libérer cette espace " | mail -s "Alerte quota" $user
-
+     
+ 	 for i in $(repquota /home | grep "+") ; do
+ 	 
+          read user used soft hard grace
+          soft=${soft//[^0-9]/}
+          used=${used//[^0-9]/}
+          hard=${hard//[^0-9]/}
+          
+          occupe=$((soft - used ))
+          echo " 0 6 * * *  echo \"Vous avez dépassé $occupe de la limite imposée , vous avez $grace pour libérer cette espace \" | mail -s \"Alerte quota \" $user"  >> cron   
+          
+         done
+          
 }
 
 
-#verification /data si soft dépassé
+#verification /data
 
-verification_data() #partition /data
+verification_soft_data() 
 {
 
-   nombre_de_depassement=$(repquota /data | grep "+" | wc -l) #soft dépassé
-   
-     if [ "$nombre_de_depassement" -eq 0 ] ; then
-            exit
-      fi     
-   
-   for i in $(repquota /data | grep "+") ; do #pour les users
-   
-          read user used soft hard grace
-          echo "Vous avez depassez la limite imposée ,vous occupé l'espace de tolérance de $soft vous avez $grace pour libérer cette espace " | mail -s "Alerte quota" $user
-   
-          
-   nombre_de_depassement=$(repquota -g /data | grep "+" | wc -l) #soft dépassé
 
-      if [ "$nombre_de_depassement" -eq 0 ] ; then
-            exit
-      fi     
-      
-   for i in $(repquota -g /data | grep "+") ; do #pour les groupes
+   
+    	for i in $(repquota /data | grep "+") ; do #pour les users
    
           read user used soft hard grace
-          echo "Vous avez depassez la limite imposée ,vous occupé l'espace de tolérance de $soft vous avez $grace pour libérer cette espace " | mail -s "Alerte quota" $user
+          soft=${soft//[^0-9]/}
+          used=${used//[^0-9]/}
+          hard=${hard//[^0-9]/}
           
+          occupe=$((soft -used))
+          
+          echo " 0 6 * * *  echo \"Vous avez dépassé $occupe de la limite imposée , vous avez $grace pour libérer cette espace \" | mail -s \"Alerte quota \" $user"  >> cron 
+         done
+   
+         for i in $(repquota -g /data | grep "+") ; do #pour les groupes
+   
+          read group used soft hard grace
+          soft=${soft//[^0-9]/}
+          used=${used//[^0-9]/}
+          hard=${hard//[^0-9]/}
+          
+           occupe=$((soft -used))
+           echo "0 6 * * *   echo \"$group dépasse $occupe de la limite imposée \" | mail -s \"Alerte quota \" root " >> cron  #envoie le mail à root 
+          done
 }
 
-verification_data
-verification_home
+verification_hard_data() 
+{
 
-#Ce script est lancé 1 fois par semaine pour voir le si l'utilisateur/groupe ne dépassent pas le quota qui leur est attribué
-#si soft atteint envoie de mail à l'utilisateur 
+     
+ 	 for i in $(repquota /data ) ; do
+ 	 
+          read user used soft hard grace
+          soft=${soft//[^0-9]/}
+          used=${used//[^0-9]/}
+          hard=${hard//[^0-9]/}
+          
+          difference=$((used - hard ))
+          
+              if [ "$difference" -lt 0 ] ; then
+                
+              echo " 0 6 * * *  echo \"Vous avez dépassé   la limite imposée \" | mail -s \"Alerte Quota  \" $user"  >> cron
+              fi   
+          
+         done
+         
+         
+          for i in $(repquota -g /data ) ; do
+ 	 
+          read group used soft hard grace
+          soft=${soft//[^0-9]/}
+          used=${used//[^0-9]/}
+          hard=${hard//[^0-9]/}
+          
+          difference=$((used - hard ))
+          
+              if [ "$difference" -lt 0 ] ; then #hard dépassé
+                
+              echo " 0 6 * * *  echo \"le groupe $groupe  dépasse  la limite imposée \" | mail -s \"Alerte Quota  \" root "  >> cron
+              fi   
+          
+         done
+          
+}
+crontab -l > cron #sauvegarder les autres cron 
+verification_home
+verification_soft_data
+verification_hard_data
+crontab  < cron # définition des cron pour les mails envoyés aux utilisateurs ou groupes (1fois/jour à 6:00)
 
